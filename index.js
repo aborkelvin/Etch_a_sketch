4ffb22d68687be237c0cbc916de118146b8d5526
Kelvin <aborkelvin@gmail.com>
2022-02-24 22:08:25 +0100

    Better interface design
    *Added title of game and some extra buttons for more experience
    *styled the new additions to appropriate colors and positions
    *improved the previous color mixings
    *Added a new function which allows coloring the divs with different
     colors

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const container = document.querySelector('.container');
 const butt = document.querySelector('.butt');
+const ink = document.querySelector('#ink');
+/*if (document.getElementById('ink').clicked==true) {
+    alert('i am m');
+}*/
+const erase =  document.querySelector('#erase');
 
-function gridbox(sqrs,col,row) { //Creates the required number of divs and styles into appropriate gridboxes
+function gridbox(sqrs,col,row) { //Creates the required number of divs and styles them into appropriate gridboxes
     for (let i = 0; i<sqrs; i++) {
     const div = document.createElement('div');
     div.classList.add('blocks');
@@ -15,10 +20,22 @@ function gridbox(sqrs,col,row) { //Creates the required number of divs and style
 
 gridbox(256,16,16);
 
-function eve() { //listens for mouse any div(gridbox) and changes the class to indicate color change
+function eve() { //listens for mouseover any div(gridbox) and changes the class to indicate color change
     function change(x){
-        document.getElementById('div'+x).className = 'hovered';
+        function rainbow(){//generates random numbers used as rgb values to create different colors
+            if(ink.clicked==true){
+                alert('mpeopel');
+                document.getElementById('div'+x).style.backgroundColor = "rgb(0,0,0)";
+            }else{
+                let r = Math.floor(Math.random()*256);
+                let g = Math.floor(Math.random()*256);
+                let b = Math.floor(Math.random()*256);
+                document.getElementById('div'+x).style.backgroundColor = `rgb(${r},${g},${b})`;
+            }
+        }
+        rainbow();
     }
+
     let num = container.children.length;
     for(let i = 0; i<num;i++){
         let c = 'cross';
@@ -48,4 +65,5 @@ function stop() {// takes user desired grids,clears the previous grid and calls 
 
 container.addEventListener('mouseover',eve); //listens for any movement over the container div and calls the functions
 butt.addEventListener('click',stop); 
- 
+//ink.addEventListener('click',);
+//erase.addEventListener('click',)
