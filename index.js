bad3a42a48c1f8d4d7e6da899e6f02ba45011c47
Kelvin <aborkelvin@gmail.com>
2022-02-24 15:18:58 +0100

    Pre final works
        For Js
    *Added a function to house creation of the required number of divs and
     their styling into appropriate gridboxes
    *Moved the styling of the grids from css to script to accomodate passing
     passing parameters into them
    *Added a function that listens for mouseover any div(gridbox) and
     changes the class to indicate color change.
    *Added the stop function which onclick of the button takes user desired
     grids,clears the previous grid and calls the div creator function with
     the user input as parameters.
    *Added a listener which listens for any movement over the container div
     and calls the functions .
        For css
    *Basically added colors and styles

--- index.js
+++ index.js
@@ -1,15 +1,68 @@
 const container = document.querySelector('.container');
+const butt = document.querySelector('.butt');
 
-for (let i = 0; i<256; i++) {
- const div = document.createElement('div');
- div.classList.add('blocks');
- div.setAttribute('id', 'div'+i);
- div.textContent = 'Hey I am '+ i;
- container.appendChild(div);
+
+
+function gridbox(sqrs,col,row) { //Creates the required number of divs and styles into appropriate gridboxes
+    for (let i = 0; i<sqrs; i++) {
+    const div = document.createElement('div');
+    div.classList.add('blocks');
+    div.setAttribute('id', 'div'+i);
+    div.textContent =  '';
+    container.appendChild(div);
+    container.style.gridTemplateColumns = `repeat(${col},1fr)`;
+    container.style.gridTemplateRows = `repeat(${row},1fr)`;
+    }
 }
-function change() {
-    let check = document.getElementsByClassName('blocks');
-    let length = document.getElementById('div4');
-    
+gridbox(256,16,16);
+
+
+
+function eve() { //listens for mouse any div(gridbox) and changes the class to indicate color change
+    function change(x){
+        document.getElementById('div'+x).className = 'hovered';
+    }
+    let num = container.children.length;
+    for(let i = 0; i<num;i++){
+        let c = 'cross';
+        let  may = c + i;
+        may = document.getElementById('div'+i);
+        may.addEventListener('mouseover',function(){change(i);})
+    }
 }
-change();
+
+
+
+
+
+
+function stop() {// takes user desired grids,clears the previous grid and calls the div creator function with the user input as parameters
+    var side = prompt('Desired number of squares per side below 90?');
+    side = Number(side);
+    var input= side * side;
+    /*for(let i = 0;i<256;i++){ //first problem: the 256 here has to be the input variable //first trial
+        const div = document.querySelector('.blocks');
+        container.removeChild(container.firstElementChild);
+    }*/
+    var num = container.children.length
+    for(let i =0;i<num;i++){
+        if(container.hasChildNodes()){
+            container.removeChild(container.children[0]);
+        }
+    }
+    gridbox(input,side,side);
+}
+
+
+
+
+
+
+
+
+
+
+
+container.addEventListener('mouseover',eve); //listens for any movement over the container div and calls the functions
+butt.addEventListener('click',stop); 
+ 
